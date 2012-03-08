60631c79de55e3d712134775b86371b721fc7cd8
Jordan Sissel <jls@semicomplete.com>
2012-03-08 10:05:54 -0800

    - fix gemspec filename

--- printrpm.rb
+++ printrpm.rb
@@ -4,16 +4,19 @@ require "rpm/file"
 
 rpm = RPM::File.new(ARGV[0])
 
-p rpm.lead
+#p rpm.lead
 rpm.signature.tags.each do |tag|
   #p :tag => [tag.tag, tag.type, tag.count, tag.value]
 end
 
 rpm.header.tags.each do |tag|
   #next unless tag.tag.to_s =~ /(payload|sig)/
-  p :tag => [tag.tag, tag.type, tag.count, tag.value]
+#  p :tag => [tag.tag, tag.type, tag.count, tag.value]
 end
 
-payload = rpm.payload
-fd = File.new("/tmp/rpm.payload", "w")
-fd.write(rpm.payload.read)
+require "awesome_print"
+ap rpm.requires
+
+#payload = rpm.payload
+#fd = File.new("/tmp/rpm.payload", "w")
+#fd.write(rpm.payload.read)
