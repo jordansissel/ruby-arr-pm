f69a5775a625a340ae4ffe5194f32006420ddb10
Andrew Kiellor <akiellor@thoughtworks.com>
2012-07-21 16:29:54 -0700

    Expand the requirements so JRuby can resolve them from jar files.

--- lib/arr-pm/file/header.rb
+++ lib/arr-pm/file/header.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), "..", "namespace")
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "namespace"))
 require File.join(File.dirname(__FILE__), "tag")
 require "cabin"
 
