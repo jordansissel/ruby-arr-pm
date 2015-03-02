68e10be849088d20c65fa6b915ee67fbd1af1677
Simas Cepaitis <simas.cepaitis@snc.lt>
2015-03-02 14:52:36 +0000

    Add new initializer to read rpm header

--- lib/arr-pm/namespace.rb
+++ lib/arr-pm/namespace.rb
@@ -1,3 +1,14 @@
 class RPM
   class File; end
+
+  def self.read(path)
+    instance = allocate
+    rpm_file = RPM::File.new(path)
+    rpm_file.header.tags.each do |tag|
+      instance.define_singleton_method(tag.tag, lambda{ tag.value} )
+    end
+
+    instance
+  end
+
 end
